242e23065d04a979a85e05ba4d790f65b6269588
Volodymyr Hil <vladimir.hil@hotmail.com>
2017-02-05 22:03:57 +0100

    implement field changes formatter

--- src/Helpfulcore.Audit/Events/AuditItemEventListener.cs
+++ src/Helpfulcore.Audit/Events/AuditItemEventListener.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using Helpfulcore.Audit.Formatters;
 using Helpfulcore.Logging;
 using Sitecore.Configuration;
 using Sitecore.Data;
@@ -12,18 +13,14 @@ namespace Helpfulcore.Audit.Events
 {
     public class AuditItemEventListener
     {
-        protected bool DisplaySystemFieldChanges
-        {
-            get { return Settings.GetBoolSetting("Helpfulcore.Audit.ItemSaved.DisplaySystemFieldChanges", false); }
-        }
-        protected bool DisplayRichTextFieldChanges
-        {
-            get { return Settings.GetBoolSetting("Helpfulcore.Audit.ItemSaved.DisplayRichTextFieldChanges", false); }
-        }
-
         protected ILoggingService Log
         {
             get { return Factory.CreateObject("helpfulcore/audit/loggingService", true) as ILoggingService; }
+        }
+
+        protected FieldChangesFormatter FieldChangesFormatter
+        {
+            get { return Factory.CreateObject("helpfulcore/audit/fieldChangesFormatter", true) as FieldChangesFormatter; }
         }
 
         public void OnItemAdded(object sender, EventArgs args)
@@ -195,25 +192,9 @@ namespace Helpfulcore.Audit.Events
                 var item = ExtractItem(args, 0);
                 var itemChanges = ExtractArgument<ItemChanges>(args, 1);
 
-                var changes = new StringBuilder();
+                var formattedChanges = this.FieldChangesFormatter.Format(itemChanges);
 
-                changes.AppendLine();
-                changes.AppendLine("Item changes:");
-                foreach (var prop in itemChanges.Properties)
-                {
-                    changes.AppendLine($"propertyName : '{prop.Value.Name}', change: [new value : '{prop.Value.Value}', old value: '{prop.Value.OriginalValue}']");
-                }
-
-                foreach (FieldChange field in itemChanges.FieldChanges)
-                {
-                    var canLog = DisplayRichText(field) || DisplaySystemField(field) || !(IsRichText(field) || IsSystemField(field));
-                    if (canLog && !IsStatField(field))
-                    {
-                        changes.AppendLine($"field name : '{field.Definition.Name}', change: [new value: '{field.Value}', old value: '{field.OriginalValue}']");
-                    }
-                }
-
-                this.Log.Audit($"{eventArgs.EventName} {this.UserAndItemInfo(item)}. {changes}", this);
+                this.Log.Audit($"{eventArgs.EventName} {this.UserAndItemInfo(item)}. {formattedChanges}", this);
             }
             catch (Exception ex)
             {
@@ -246,6 +227,7 @@ namespace Helpfulcore.Audit.Events
                 var templateChange = ExtractArgument<TemplateChangeList>(args, 1);
                 var oldTemplate = templateChange.Source;
                 var newTemplate = templateChange.Target;
+
                 this.Log.Audit($"{eventArgs.EventName} {this.UserAndItemInfo(item)}. Original template: {oldTemplate.ID}, new template: {newTemplate}", this);
 
                 this.LogAuditMessage(eventArgs.EventName, item);
@@ -334,34 +316,6 @@ namespace Helpfulcore.Audit.Events
         private string UserAndItemInfo(Item item)
         {
             return $"{this.UserInfo()} {this.ItemInfo(item)}";
-        }
-
-        private bool IsStatField(FieldChange field)
-        {
-            return field.Definition.Name.Equals("__Updated", StringComparison.CurrentCultureIgnoreCase)
-                   || field.Definition.Name.Equals("__Created", StringComparison.CurrentCultureIgnoreCase)
-                   || field.Definition.Name.Equals("__Revision", StringComparison.CurrentCultureIgnoreCase)
-                   || field.Definition.Name.Equals("__Updated by", StringComparison.CurrentCultureIgnoreCase);
-        }
-
-        private bool IsSystemField(FieldChange field)
-        {
-            return field.Definition.Name.StartsWith("__");
-        }
-
-        private bool IsRichText(FieldChange field)
-        {
-            return field.Definition.Type.ToLower().Equals("rich text", StringComparison.CurrentCultureIgnoreCase);
-        }
-
-        private bool DisplaySystemField(FieldChange field)
-        {
-            return IsSystemField(field) && this.DisplaySystemFieldChanges;
-        }
-
-        private bool DisplayRichText(FieldChange field)
-        {
-            return IsRichText(field) && this.DisplayRichTextFieldChanges;
         }
     }
 }
---
+++ src/Helpfulcore.Audit/Formatters/FieldChangesFormatter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data.Items;
+
+namespace Helpfulcore.Audit.Formatters
+{
+    public class FieldChangesFormatter
+    {
+        public FieldChangesFormatter()
+        {
+            this.IgnoreFieldTypes = new List<string>();
+            this.IgnoreFields = new List<string>();
+        }
+
+        public bool IgnoreSystemFields { get; protected set; }
+        public List<string> IgnoreFieldTypes { get; protected set; }
+        public List<string> IgnoreFields { get; protected set; }
+
+        public string Format(ItemChanges itemChanges)
+        {
+            var changes = new StringBuilder();
+
+            changes.AppendLine();
+            changes.AppendLine("Item changes:");
+
+            var propOrFieldChangesLogged = false;
+            foreach (var prop in itemChanges.Properties)
+            {
+                changes.AppendLine($"propertyName : '{prop.Value.Name}', change: [new value : '{prop.Value.Value}', old value: '{prop.Value.OriginalValue}']");
+                propOrFieldChangesLogged = true;
+            }
+
+            foreach (FieldChange field in itemChanges.FieldChanges)
+            {
+                if (this.IgnoreSystemFields && this.IsSystemField(field)) continue;
+                if (this.IsIgnoredField(field) || this.IsIgnoredFieldType(field)) continue;
+
+                changes.AppendLine($"field name : '{field.Definition.Name}', change: [new value: '{field.Value}', old value: '{field.OriginalValue}']");
+                propOrFieldChangesLogged = true;
+            }
+
+            if (!propOrFieldChangesLogged)
+            {
+                changes.AppendLine("Field changes may have been ignored to log in the configration.");
+            }
+
+            return changes.ToString();
+        }
+
+        private bool IsIgnoredFieldType(FieldChange field)
+        {
+            return this.IgnoreFieldTypes.Any(fieldName => field.Definition.Type.ToLower().Equals(fieldName, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        private bool IsIgnoredField(FieldChange field)
+        {
+            return this.IgnoreFields.Any(fieldName => field.Definition.Name.ToLower().Equals(fieldName, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        private bool IsSystemField(FieldChange field)
+        {
+            return field.Definition.Name.StartsWith("__");
+        }
+    }
+}
+
